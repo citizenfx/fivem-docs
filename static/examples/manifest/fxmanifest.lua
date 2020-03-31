5ded34f5ffd039604d24cb6e4aa69300caa5bc69
Andre Tabarra <tabarra@vexguard.net>
2020-03-31 02:29:52 -0300

    Added author, version and description to resources

--- static/examples/manifest/fxmanifest.lua
+++ static/examples/manifest/fxmanifest.lua
@@ -1,14 +1,19 @@
+-- Resource Metadata
 fx_version 'bodacious'
 games { 'rdr3', 'gta5' }
 
+author 'Jon Doe'
+description 'Example resource'
+version '1.0.0'
+
+-- What to run
 client_scripts {
     'client.lua',
     'client_two.lua'
 }
-
 server_script 'server.lua'
 
--- extra data can be used as well
+-- Extra data can be used as well
 my_data 'one' { two = 42 }
 my_data 'three' { four = 69 }
 
